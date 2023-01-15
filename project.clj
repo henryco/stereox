369aefd2dcf638d7c79700aa69359b871b63a0ca
Henry Domagalski <henrycodev@gmail.com>
2023-01-15 02:24:15 +0100

    wip

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject stereox "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject stereox "0.0.1"
+  :description "Stereo vision"
   :url "https://tindersamurai.dev"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
+  :injections [(nu.pattern.OpenCV/loadShared)]
   :repl-options {:init-ns stereox.core}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [cljfx "1.7.22"]
-
-                 ])
+                 [org.openpnp/opencv "4.6.0-0"]
+                 [cljfx "1.7.22"]])
--- src/stereox/core.clj
+++ src/stereox/core.clj
@@ -1,15 +1,18 @@
 (ns stereox.core
-  (:require [cljfx.api :as fx]))
+  (:require [cljfx.api :as fx])
 
-(fx/on-fx-thread
-  (fx/create-component
-    {:fx/type :stage
-     :showing true
-     :title "Cljfx example"
-     :width 300
-     :height 100
-     :scene {:fx/type :scene
-             :root {:fx/type :v-box
-                    :alignment :center
-                    :children [{:fx/type :label
-                                :text "Hello world"}]}}}))
+  )
+
+;(fx/on-fx-thread
+;  (fx/create-component
+;    {:fx/type :stage
+;     :showing true
+;     :title "Cljfx example"
+;     :width 300
+;     :height 100
+;     :scene {:fx/type :scene
+;             :root {:fx/type :v-box
+;                    :alignment :center
+;                    :children [{:fx/type :label
+;                                :text "Hello world"}]}}}))
+
