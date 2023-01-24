4f96467cac278fe0c10b4912551c10e90a51a4e5
Henry Domagalski <henrycodev@gmail.com>
2023-01-24 01:24:08 +0100

    #wip

--- src/stereox/calibration/calibration.clj
+++ src/stereox/calibration/calibration.clj
@@ -4,21 +4,32 @@
   (:import (java.io ByteArrayInputStream File)
            (javafx.animation AnimationTimer)
            (javafx.application Platform)
-           (javafx.scene.image Image))
+           (javafx.scene.image Image)
+           (org.opencv.calib3d Calib3d)
+           (org.opencv.core Mat MatOfPoint2f Size TermCriteria)
+           (org.opencv.imgproc Imgproc))
   (:gen-class))
 
-(def ^:private *window
-  (atom {:alive  true
-         :width  nil
-         :height nil
-         }))
+(defrecord Props
+  [^File directory
+   ^Integer rows
+   ^Integer columns])
+
+(def ^:private *params
+  "Props"
+  (atom nil))
 
 (def ^:private *camera
+  "StereoCamera"
   (atom nil))
 
 (def ^:private *state
+  "JavaFX UI State"
   (atom {:title  "StereoX calibration"
          :scale  1.
+         :alive  true
+         :width  nil
+         :height nil
          :camera {:viewport {:width 0 :height 0 :min-x 0 :min-y 0}
                   :image    ^Image []
                   }
@@ -40,7 +51,7 @@
 
 (defn shutdown [& {:keys [code]}]
   (try
-    (swap! *window assoc :alive false)
+    (swap! *state assoc :alive false)
     (camera/release @*camera)
     (catch Exception e (.printStackTrace e)))
   (try
@@ -52,8 +63,8 @@
   (let [s (-> @*state :scale)
         ow (-> @*state :camera :viewport :width)
         oh (-> @*state :camera :viewport :height)
-        ww (-> @*window :width)
-        hh (-> @*window :height)]
+        ww (-> @*state :width)
+        hh (-> @*state :height)]
     (if (and (some? ww) (some? hh))
       (let [[dw dh] (map - [ww hh] [(* ow s 2) (* oh s)])]
         (if (< dw dh)
@@ -63,11 +74,11 @@
       )))
 
 (defn on-win-height-change [v]
-  (swap! *window assoc :height v)
+  (swap! *state assoc :height v)
   (on-win-change))
 
 (defn on-win-width-change [v]
-  (swap! *window assoc :width v)
+  (swap! *state assoc :width v)
   (on-win-change))
 
 (defn render-images [{:keys [camera scale]}]
@@ -110,21 +121,85 @@
   (.start
     (proxy [AnimationTimer] []
       (handle [_]
-        (if (:alive @*window)
+        (if (:alive @*state)
           (func))
         ))))
 
+(defrecord CBData
+  [^Mat image_original
+   ^Mat image_chessboard
+   ^Boolean found
+   ^MatOfPoint2f corners])
+
+(defn find-squares ^CBData [^Mat image]
+  (let [output_image (Mat.)
+        corners (MatOfPoint2f.)
+        flags (+ Calib3d/CALIB_CB_ADAPTIVE_THRESH
+                 Calib3d/CALIB_CB_NORMALIZE_IMAGE
+                 Calib3d/CALIB_CB_FAST_CHECK)
+        pattern_size (Size. (- (:rows @*params) 1)
+                            (- (:columns @*params) 1))
+        found (Calib3d/findChessboardCorners image
+                                             pattern_size
+                                             corners
+                                             flags)]
+    ; TODO GRAYSCALE
+    (.copyTo image
+             output_image)
+    (if found
+      (Imgproc/cornerSubPix output_image
+                            corners
+                            (Size. 11 11)
+                            (Size. -1 -1)
+                            (TermCriteria. (+ TermCriteria/MAX_ITER
+                                              TermCriteria/COUNT)
+                                           30
+                                           0.1)))
+    (Calib3d/drawChessboardCorners output_image
+                                   pattern_size
+                                   corners
+                                   found)
+    (CBData. image
+             output_image
+             found
+             corners)))
+
+(defn prepare-images
+  "Founds chessboard pattern on the image.
+  Expects vector or sequence of org.opencv.core.Mat
+  Returns vector or sequence of CBData"
+  [matrices]
+  (if (some? matrices)
+    (map #(deref %)
+         (map #(future (find-squares %))
+              matrices))))
+
+(defn unwrap-prepared
+  "Unwrap prepared data sequence returning images.
+  Expects vector or sequence of CBData
+  Returns vector of org.opencv.core.Mat"
+  [data]
+  (if (some? data)
+    (map #(:image_chessboard %) data)))
+
 (defn main-cb []
   (let [captured (camera/capture @*camera)
-        images (image-adapt captured)]
+        prepared (prepare-images captured)
+        images (image-adapt (unwrap-prepared prepared))]
     (if (some? images)
       (swap! *state assoc-in [:camera :image] images))
     ; TODO: MORE
     )
   )
 
-(defn calibrate [& {:keys [output-folder width height] :as all}]
-  (prep-dirs output-folder)
+(defn calibrate [& {:keys [output-folder
+                           columns
+                           rows
+                           width
+                           height
+                           ] :as all}]
+  ; setup calibration properties
+  (reset! *params (Props. output-folder rows columns))
 
   ; setup camera
   (reset! *camera (camera/create all))
@@ -136,5 +211,4 @@
   ;; Convenient way to add watch to an atom + immediately render app
   (fx/mount-renderer *state renderer)
 
-  (start-ui-loop main-cb)
-  )
+  (start-ui-loop main-cb))
